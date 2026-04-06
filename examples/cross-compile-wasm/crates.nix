88117c2091b12cb185580a3e5f4e5cededde56ec
dawn <90008@gaze.systems>
2026-04-06 20:35:22 +0300

    fix(examples): use dart sass on wasm example

--- examples/cross-compile-wasm/crates.nix
+++ examples/cross-compile-wasm/crates.nix
@@ -21,20 +21,20 @@
       targets."wasm32-unknown-unknown" = {
         default = true;
         drvConfig.env = {
-          TRUNK_TOOLS_SASS = pkgs.nodePackages.sass.version;
+          TRUNK_TOOLS_SASS = pkgs.dart-sass.version;
           TRUNK_TOOLS_WASM_BINDGEN = wasm-bindgen-cli.version;
           TRUNK_TOOLS_WASM_OPT = "version_${lib.removeSuffix "_b" pkgs.binaryen.version}";
           TRUNK_SKIP_VERSION_CHECK = "true";
         };
         drvConfig.mkDerivation = {
           # add trunk and other dependencies
-          nativeBuildInputs = [pkgs.trunk pkgs.nodePackages.sass wasm-bindgen-cli pkgs.binaryen];
+          nativeBuildInputs = [pkgs.trunk pkgs.dart-sass wasm-bindgen-cli pkgs.binaryen];
           # override build phase to build with trunk instead
           buildPhase = ''
             echo sass is version $TRUNK_TOOLS_SASS
             echo wasm bindgen is version $TRUNK_TOOLS_WASM_BINDGEN
             HOME=$TMPDIR \
-              trunk -v build \
+              trunk --offline -v build \
               --dist $out \
               --release \
               ''${cargoBuildFlags:-}
